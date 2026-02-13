7d7affef28f47c98463875b02faef7ee3a1819d5
baghawan <dianbaghawanputera@gmail.com>
2026-02-13 20:41:44 +0700

    feat(homepage): controller

--- src/api/homepage/controllers/homepage.js
+++ src/api/homepage/controllers/homepage.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * homepage controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::homepage.homepage');
+module.exports = createCoreController("api::homepage.homepage", () => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        works: {
+          fields: ["title", "slug", "year", "createdAt", "updatedAt"],
+          populate: {
+            expertise: {
+              fields: ["name"],
+            },
+            gallery: {
+              fields: ["name", "alternativeText", "url", "width", "height"],
+            },
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  },
+}));
